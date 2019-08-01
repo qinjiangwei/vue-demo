2bda03dfddeae2cc53db2cc55b9774e54f5df97a
qinjiangwei <qinjiangweili@foxmail.com>
2019-08-01 18:17:58 +0800

    Initial commit

---
+++ src/pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <a v-on:click="doSomething">你好世界</a>
+</template>
+
+<script>
+    export default {
+      name: "login",
+      methods:{
+        async doSomething(){
+           console.debug("hello world")
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
